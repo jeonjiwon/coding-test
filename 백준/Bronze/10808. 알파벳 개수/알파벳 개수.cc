1deda1617e5fea7a0736c02e46d91348742bd114
jeonjiwon <jiwon9609@naver.com>
2025-03-17 23:39:31 +0900

    ⚡️ [refactor] 개선 코드 추가

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -1,12 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 int cnt[26];
+// int cnt[123];
 string str;
 int main(void){
 	
 	//counting star는 맵 또는 배열 !!!(무조건)
-	// MAP-> string 기반 
-	// ARRAY -> Int  
+
+	// 배열과 맵을 언제 쓰느냐!
+	// 1) MAP-> string 기반
+	// 2) ARRAY -> Integer 기반 
+	//    (10만, 100만, 1000만 이런식으로 포인터 방식으로 들어오면 맵을 써야한다)  
+	
 	// 아스키 코드 'A' 65 / 'a' 97 
 	
 	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
@@ -14,9 +19,26 @@ int main(void){
 	cin >> str;
 	
 	for(char a : str){
-		cnt[a - 'a']++;
+		cnt[a - 'a']++;  //좌표 이동
+		// cnt[a]++;  //97개의 공간 낭비로 인해 위처럼 구현한다.
 	}
 	
 	for(int i=0; i<26; i++) cout << cnt[i] << " ";
+
+
+	// 시간복잡도 낭비..아래 처럼 구현하지 말것 ! 
+	// string str;
+    // cin >> str;
+    // int cnt = int('z') - int('a') + 1;
+    // int a[cnt] = {0,};
+    // for(int i = 0; i < str.size(); i++) {
+    //     for(int j=0; j<cnt; j++) {
+    //         if((int)str[i] == int('a') + j) {
+    //             a[j]++;
+    //             break;
+    //         }
+    //     }
+    // }
+
 	return 0;
 }
--- 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
@@ -1,37 +1,58 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
 using namespace std;
-int a[101] = {0,};
-int main(){
+
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int sum_a =0, sum_b = 0, sum_c = 0;
-    int p_a = 0, p_b = 0, p_c = 0;
-    cin >> p_a;
-    cin >> p_b;
-    cin >> p_c;
 
-    int n1, n2 = 0;
-    for(int i = 0; i < 3; i++){
-        cin >> n1 >> n2;        
-        for(int j=n1; j<n2; j++){
-            a[j]++;
+    int p[100]  = {0, };
+    int cost[3] = {0, };
+    int sum[3]  = {0, };
+
+    cin >> cost[0] >> cost[1] >> cost[2];
+
+    int maxLen = 0;
+    int start = 0, end = 0;
+
+    // 1 - 내가 푼 풀이
+    // for(int i=0; i<3; i++) {
+    //     cin >> start >> end;
+    //     if(maxLen < end) {
+    //         maxLen = end;
+    //     }
+    //     for(int j=start; j<end; j++) { // 이상 ~ 미만
+    //         p[j]++;
+    //     }
+    // }
+
+    // 2 - 개선 풀이  "누적합"을 좀더 활용하여 이중for 문 사용x 
+    for(int i=0; i<3; i++) {
+        cin >> start >> end;
+        if(maxLen < end) {
+            maxLen = end;
         }
+        p[start]++;  // 시작 지점 증가
+        p[end]--;  // 끝 다음 지점 감소 
+    }
+    
+    for(int i=1; i<=maxLen; i++) {
+        p[i] += p[i-1];
     }
 
-    for(int i=1; i<101; i++){
-        if(a[i] == 1){
-            sum_a++;
+    for(int i=0; i<maxLen; i++) {
+        if(p[i] == 1) {
+            sum[0]++;
         }
-        if(a[i] == 2){
-            sum_b++;
+        if(p[i] == 2) {
+            sum[1]++;
         }
-        if(a[i] == 3){
-            sum_c++;
+        if(p[i] == 3) {
+            sum[2]++;
         }
     }
-    cout << (sum_a * p_a) + (sum_b * 2 *p_b) + (sum_c *3* p_c) << endl;
-    return 0;
+     
+    cout << (sum[0] * cost[0]) + (sum[1] * cost[1] * 2) + (sum[2] * cost[2] * 3) << endl;
 }
