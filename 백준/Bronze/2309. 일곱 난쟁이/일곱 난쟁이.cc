428b90751901aa9e4dd8d66390943608111a980d
jeonjiwon <jiwon9609@naver.com>
2025-03-17 22:43:03 +0900

    ⚡️ [refactor] 순열 직접 구현해서 풀이 추가

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,28 +1,69 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
-int a[9];
-int sum;
 
-int main(void){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	for(int i=0; i<9; i++){
-		cin >> a[i];
-	}	
-	sort(a, a+9);
-	do{
-		int sum = 0;
-		for(int i=0; i<7; i++){
-			sum += a[i];
-		}
-		if(sum == 100) break;
-	} while(next_permutation(a, a+9));
-	
-	for(int i=0; i<7; i++){
-		cout << a[i] << "\n";
-	}
-	
-	 
-	return 0;
+vector<int> a;
+
+// 9, 7, 0
+void makePermutation(int n, int r, int depth) {
+    int sum = 0;
+    if(depth == r) {
+        for(int i = 0 ; i < r; i ++) {
+            // cout << a[i] << ' ';
+            sum += a[i];
+        }
+        // cout << '\n';
+
+        if(sum == 100) {
+            for(int i = 0 ; i < r; i ++) {
+                cout << a[i] << ' ';
+
+            }
+            exit(0);
+        }
+        return;
+    }
+
+    for(int i=depth; i<n; i++) {
+        swap(a[depth], a[i]);
+        makePermutation(n, r, depth + 1);
+        swap(a[depth], a[i]);
+    }
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+
+    // case 1 : Permutation 직접 구현
+
+    int num;
+    for(int i = 0 ; i < 9; i ++) {
+        cin >> num;
+        a.push_back(num);
+    }
+
+    sort(a.begin(), a.end());
+    int sum = 0 ;
+    makePermutation(9, 7, 0);
+
+    // case 2: next Permutaion 사용하기 
+    // do {
+    //     sum = 0 ;
+    //     for(int i = 0 ; i < 7; i ++) {
+    //         sum += a[i];
+    //     }
+
+    //     if(sum == 100) {
+    //         for(int i = 0 ; i < 7; i ++) {
+    //             cout << a[i] << ' ';
+    //         }
+    //         break;
+    //     };
+    // } while (next_permutation(a.begin(), a.end()));
+    
 }
