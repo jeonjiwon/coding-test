f78199fb2035f4ced25cdd1121432f43627aa1bf
jeonjiwon <jiwon9609@naver.com>
2025-03-20 01:00:54 +0900

    ⚡️ [Refactor] 코드 수정

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -1,22 +1,33 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+// #include <sstream>
+#include <string>
 using namespace std;
-string str ; 
-int main(void){
-	ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-	
-	getline(cin, str);
 
-	for(int i=0; i<str.size(); i++){
-		if (str[i] >= 65 && str[i] <= 90){ //upper 
-			if((int)str[i] + 13 > 90) str[i] = (int)str[i] - 13;
-			else str[i] = (int)str[i] + 13;
-		}
-        else if (str[i] >= 97 && str[i] <= 122){ // lower
-			if((int)str[i] + 13 > 122) str[i] = (int)str[i] - 13;
-			else str[i] = (int)str[i] + 13;
-		}
-		cout << str[i];
-	}
-	
-	return 0;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    string str;
+    getline(cin, str); // 공백까지 입력받기 위해 getline 사용
+
+    int n = 0;
+    for(char c : str) {
+        n = (int)c + 13;
+        if(c >= 'A' && c <= 'Z') {
+            if(n > (int)'Z'){
+                n = n - 26;
+            }
+            cout << (char)n;
+        } else if(c >= 'a' && c <= 'z') {
+            if(n > (int)'z'){
+                n = n - 26;
+            }
+            cout << (char)n;
+        } else {
+            cout << c;
+        }
+    }
 }
